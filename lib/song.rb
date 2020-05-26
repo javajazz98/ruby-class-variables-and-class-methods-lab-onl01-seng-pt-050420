d5c05eabf74848ff80f412057a7a0c150b886129
jassmine tatum <jassmine.tatum@gmail.com>
2020-05-26 00:07:20 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,54 +1,42 @@
 class Song
-  attr_accessor :name, :artist, :genre
+    attr_accessor :name, :artist, :genre
 
-  @@count = 0
-  @@genres = []
-  @@artists = []
+    @@count = 0
+    @@artists = []
+    @@genres = []
 
-  def initialize(name, artist, genre)
-    @name = name
-    @artist = artist
-    @genre = genre
-    @@count += 1
-    @@genres << self.genre
-    @@artists << self.artist
-  end
-
-  def self.count
-    @@count
-  end
-
-  def self.artists
-    @@artists.uniq
-  end
-
-  def self.genres
-    @@genres.uniq
-  end
-
-  def self.genre_count
-    song_hash = {}
-    @@genres.each do |genre|
-      if song_hash[genre]
-        song_hash[genre] += 1
-      else
-        song_hash[genre] = 1
-      end
+    def initialize( name, artist, genre)
+        @name = name
+        @artist = artist
+        @genre = genre
+        @@count += 1
+        @@artists << artist
+        @@genres << genre
     end
-    song_hash
-  end
 
-  def self.artist_count
-    artist_hash = {}
-    @@artists.each do |artist|
-      if artist_hash[artist]
-        artist_hash[artist] += 1
-      else
-        artist_hash[artist] = 1
-      end
+    def self.count
+        @@count
     end
-    artist_hash
-  end
+
+    def self.artists
+        @@artists.uniq
+    end
+
+    def self.genres
+        @@genres.uniq
+    end
+
+    def self.artist_count
+        hash = Hash.new(0)
+        @@artists.each {|i| hash[i] += 1 }
+        hash
+    end
+
+    def self.genre_count
+        hash = Hash.new(0)
+        @@genres.each {|i| hash[i] += 1 }
+        hash
+    end
 
 
 end
